f588c660f4fe0aed278dc1492e10d16412ca3493
AlexDjangoX <alexmonk17@gmail.com>
2023-05-15 15:53:49 +0200

    FEATURE: Insights.jsx

--- sections/World.jsx
+++ sections/World.jsx
@@ -3,9 +3,9 @@
 import { motion } from 'framer-motion';
 
 import styles from '../styles';
-import { newFeatures } from '../constants';
-import { NewFeatures, TitleText, TypingText } from '../components';
-import { planetVariants, staggerContainer, fadeIn } from '../utils/motion';
+
+import { TitleText, TypingText } from '../components';
+import { staggerContainer, fadeIn } from '../utils/motion';
 
 const World = () => (
   <section className={`${styles.paddings} relative z-10`}>
@@ -26,17 +26,33 @@ const World = () => (
         className="relative mt-[68px] flex w-full h-[450px]"
       >
         <img src="/map.png" alt="map" className="w-full h-full object-cover" />
-        <div className="absolute top-[10%] right-[35%] w-[40px] h-[40px] rounded-full bg-[#5d6680]">
-          <img src="/people-01.png" alt="people" className="w-full h-full" />
+        <div className="absolute top-[10%] right-[35%] w-[75px] h-[45px]  rounded-full bg-[#e50f36]">
+          <img
+            src="/people-01.png"
+            alt="people"
+            className="rounded-full w-[40px] h-[40px]"
+          />
         </div>
-        <div className="absolute top-[45%] right-[65%] w-[40px] h-[40px] rounded-full bg-[#5d6680]">
-          <img src="/people-02.png" alt="people" className="w-full h-full" />
+        <div className="absolute top-[45%] right-[65%] w-[75px] h-[45px]  rounded-full bg-[#e50f36]">
+          <img
+            src="/people-02.png"
+            alt="people"
+            className="rounded-full w-[40px] h-[40px]"
+          />
         </div>
-        <div className="absolute top-[30%] right-[20%] w-[40px] h-[40px] rounded-full bg-[#5d6680]">
-          <img src="/people-03.png" alt="people" className="w-full h-full" />
+        <div className="absolute bottom-[25%] right-[5%] w-[75px] h-[45px]  rounded-full bg-[#e50f36]">
+          <img
+            src="/people-03.png"
+            alt="people"
+            className="rounded-full w-[40px] h-[40px]"
+          />
         </div>
-        <div className="absolute top-[30%] left-[10%] w-[40px] h-[40px] rounded-full bg-[#5d6680]">
-          <img src="/people-04.png" alt="people" className="w-full h-full" />
+        <div className="absolute top-[30%] left-[30%] w-[75px] h-[45px]  rounded-full bg-[#e50f36]">
+          <img
+            src="/people-04.png"
+            alt="people"
+            className="rounded-full w-[40px] h-[40px]"
+          />
         </div>
       </motion.div>
     </motion.div>
