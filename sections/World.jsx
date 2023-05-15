0382b66259cbae0b426cdd76367a0ef4d9f23650
AlexDjangoX <alexmonk17@gmail.com>
2023-05-15 15:33:15 +0200

    FEATURE: World.jsx

--- sections/World.jsx
+++ sections/World.jsx
@@ -1,8 +1,45 @@
 'use client';
 
+import { motion } from 'framer-motion';
+
+import styles from '../styles';
+import { newFeatures } from '../constants';
+import { NewFeatures, TitleText, TypingText } from '../components';
+import { planetVariants, staggerContainer, fadeIn } from '../utils/motion';
+
 const World = () => (
-  <section>
-    World section
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewPort={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex  flex-col `}
+    >
+      <TypingText title="| People of the Universe" textStyles="text-center" />
+      <TitleText
+        title={<>Track friends around Planet Earth</>}
+        textStyles="text-center"
+      />
+      <motion.div
+        variants={fadeIn('up', 'tween', 0.3, 1)}
+        className="relative mt-[68px] flex w-full h-[450px]"
+      >
+        <img src="/map.png" alt="map" className="w-full h-full object-cover" />
+        <div className="absolute top-[10%] right-[35%] w-[40px] h-[40px] rounded-full bg-[#5d6680]">
+          <img src="/people-01.png" alt="people" className="w-full h-full" />
+        </div>
+        <div className="absolute top-[45%] right-[65%] w-[40px] h-[40px] rounded-full bg-[#5d6680]">
+          <img src="/people-02.png" alt="people" className="w-full h-full" />
+        </div>
+        <div className="absolute top-[30%] right-[20%] w-[40px] h-[40px] rounded-full bg-[#5d6680]">
+          <img src="/people-03.png" alt="people" className="w-full h-full" />
+        </div>
+        <div className="absolute top-[30%] left-[10%] w-[40px] h-[40px] rounded-full bg-[#5d6680]">
+          <img src="/people-04.png" alt="people" className="w-full h-full" />
+        </div>
+      </motion.div>
+    </motion.div>
   </section>
 );
 
